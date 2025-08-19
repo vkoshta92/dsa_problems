02a1cfbf91015cf0529c64d416eca8e4d471e74e
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2025-08-19 23:04:32 +0530

    palandrome and reverse

---
+++ 7_palendrome.js
@@ -0,0 +1,23 @@
+
+var isPalindrome = function(x) {
+    if(x<0) return false;
+let xCopy=x;
+// x= Math.abs(x);
+    let reverseNumber= 0;
+
+
+    while(x>0){
+    
+let reminder= x%10;
+reverseNumber= (reverseNumber*10)+reminder;
+x= Math.floor(x/10);
+    }
+    // if( reverseNumber=== xCopy){
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+    return reverseNumber=== xCopy ;
+};
+
