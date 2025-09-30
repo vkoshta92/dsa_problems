cdff9f12461aec77f459b4fa78673eab8a03429d
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2025-09-30 08:17:00 +0530

    linked list roatae

--- 33_141_linked-list-cycle.js
+++ 33_141_linked-list-cycle.js
@@ -1,5 +1,35 @@
 
 // https://leetcode.com/problems/linked-list-cycle/
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  */
+
+// /**
+//  * @param {ListNode} head
+//  * @return {boolean}
+//  */
+// var hasCycle = function(head) {
+// let seenNodes= new Set();
+//     // cycleic me null kbhi nhi hota hai.
+//     //and node value repeat ho jae to 
+//   let curr = head;
+//   while(curr){
+//     if(seenNodes.has(curr)){
+//         return true;
+//     }
+//     seenNodes.add(curr);
+//     curr= curr.next;
+//   }
+
+// return false;
+    
+// };
+
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -8,23 +38,27 @@
  * }
  */
 
+
+
 /**
  * @param {ListNode} head
  * @return {boolean}
  */
 var hasCycle = function(head) {
-let seenNodes= new Set();
-    // cycleic me null kbhi nhi hota hai.
-    //and node value repeat ho jae to 
-  let curr = head;
-  while(curr){
-    if(seenNodes.has(curr)){
-        return true;
+// Floyd’s Cycle Detection algorthim;
+if (!head) return false; // head null hai empty h node list
+let slow= head;
+let fast= head.next;
+while(slow!== fast){
+    if(fast == null || fast.next== null){
+        return false;
     }
-    seenNodes.add(curr);
-    curr= curr.next;
-  }
+    slow= slow.next;
+    fast= fast.next.next;
+}
+return true;
 
-return false;//cycle nhi h
-    
 };
+
+// T- O(n);
+// S-O(1)
