ae8b745745c9a84750561134a9e1c8df3cae503a
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2026-02-18 07:01:57 +0530

    guess number

--- 74_69_binary_search_sqrt.js
+++ 74_69_binary_search_sqrt.js
@@ -15,25 +15,25 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    if(x<2) return x;
-      let left=2;
-        let right=Math.floor(x/2);
-        while(left<=right){
+var mySqrt = function (x) {
+    if (x < 2) return x;
+    let left = 2;
+    let right = Math.floor(x / 2);
+    while (left <= right) {
         // let mid= Math.floor((left+right)/2);
-        let mid= left+Math.floor((right-left)/2); // overcome overflow in other language
+        let mid = left + Math.floor((right - left) / 2); // overcome overflow in other language
 
-        if(x===mid**2){
+        if (x === mid ** 2) {
             return mid;
         }
-        else if(x>mid**2){
-            left= mid+1;
+        else if (x > mid ** 2) {
+            left = mid + 1;
         }
-        else{
-            right= mid-1;
+        else {
+            right = mid - 1;
         }
 
-        }
+    }
     return right  // sbse close yhi hogi 
-    
+
 };
